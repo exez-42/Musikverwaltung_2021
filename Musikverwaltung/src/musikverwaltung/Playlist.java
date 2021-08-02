7c19b5fe589a2f1a96ba29aa49c8979d0e5398ff
charlynuts <76880060+charlynuts@users.noreply.github.com>
2021-08-02 14:08:43 +0200

    Update Playlist.java

--- Musikverwaltung/src/musikverwaltung/Playlist.java
+++ Musikverwaltung/src/musikverwaltung/Playlist.java
@@ -4,37 +4,236 @@ import java.util.ArrayList;
 
 public class Playlist {
 
-	
+	/*
+	 * Arraylist die alle erstellten Playlisten speichert
+	 */
 	private static ArrayList<Playlist> all_pl = new ArrayList<Playlist>();
-	private String pl_name;
+	
+	
+	/*
+	 * Hilfsvariable.
+	 * Speichert den track ab der als letzts gespielt hat.
+	 * mit dieser Variable kann man bei Playlist wechsel den laufenden Track stoppen.
+	 */
+	static String previos_track;
+	
+	
+	/*
+	 * Name der Playlist & ID
+	 * -> kein Name wird doppelt vergeben 
+	 * -> somit eindeutig
+	 */
+	private String pl_name;				
+	
+	/*
+	 * Arraylist die alle Titel der Playlist abseichert 
+	 */
 	private ArrayList<Titel> playlist;
 	
 	
+	/*
+	 * Gibt an welcher Index in der Playlist als letztes gespielt wurde
+	 * -> macht vorheriger & nächster Track möglich
+	 * 
+	 */
+	private int current_titel = 0; 	//implementieren
 	
 	
 	
-	public Playlist(String pl_name) {
+	/*
+	 * Privater Kontruktor
+	 * 
+	 */
+	private Playlist(String pl_name) {
 		
 		this.pl_name = pl_name;
+		
 		playlist = new ArrayList<Titel>();
 		
+		
 		all_pl.add(this);
+	}
+
+	
+	/*
+	 * indirekter Konstruktor
+	 * prüft ob Playlist name schon exestiert 
+	 * wenn ja 
+	 * 		-> Playlist wird nicht erzeugt
+	 * 		-> return true
+	 * wenn nein:
+	 * 		-> Playlist wird erzeugt
+	 * 		-> return true
+	 */
+	static boolean create_Playlist(String pl_name) {
+	boolean name_vergeben = false;
+	for(int i = 0; i < all_pl.size(); i++) {
+		
+	if(all_pl.get(i).getPl_name() == pl_name) {
+		
+		name_vergeben = true;
+		
+	}	
+			
+	}
+	
+	
+	if(name_vergeben == true) {
+		
+		return false;
+		
+		
+	}else {
+		
+		
+		new Playlist(pl_name);
+		return true;
+		
+		
+	}	
+	}
+
+	
+	/*
+	 * Liefert geuschte Playlist zurück mit übergebenen eindeutigen Playlist namen
+	 * 
+	 */
+	static Playlist get_playlist_wID(String pl_name) {
+		
+		for(int i = 0; i < all_pl.size(); i++) {
+			
+		if(all_pl.get(i).getPl_name() == pl_name) {
+			
+			return all_pl.get(i);
+		}
+		
+		}
+		
+		
+		return null;
+		}
+	
+	
+	/*
+	 * Mehtode für Combobox
+	 * return -> String Feld mit allen PLaylist namen 
+	 */
+ 	public static String[] get_all_plname_string() {
+		
+		String[] tmp = new String[all_pl.size()]; 	
+		for (int i = 0; i < all_pl.size(); i++) {
+		tmp[i] = all_pl.get(i).getPl_name();
+	
+	}
+		
+		
+		return tmp;
+	}
+	
+	
+	
+ 	
+ 	
+	/*
+	 * return : ausgewählte PLaylist von ComboBox
+	 * 
+	 */
+	public static Playlist  get_current_playlist(String pl_name) {
+		
+		
+		for (int i = 0; i < all_pl.size(); i++) {
+			
+		if(all_pl.get(i).getPl_name() == pl_name) {return all_pl.get(i);}	
+			
+			
+			
+		}
+		
+		
+		
+		
+		return null;  
+	
+		
+		
+	}
+	
+	
+	
+	/*return aktuellen Playlist Titel 
+	 * 
+	 */
+	public Titel get_current_track() {
+		
+		
+		
+		return playlist.get(current_titel);
+		
+	}
+	
+	
+	
+	/*
+	 * aktualisiert current_titel variable +1 
+	 * 
+	 */
+	public void next_track() {
+	if(current_titel == (playlist.size()-1)) {
+		
+		current_titel = 0;
+		
+		
+	}else { 
+		
+		current_titel += 1;
+		
+	}
+		
+		
+		
+	}
+	
+	
+	/*
+	 * aktualisiert current_titel variable -1
+	 * 
+	 */
+	public void previos_track() {
+		
+		if(current_titel == 0) {
+			
+			current_titel = (playlist.size() - 1);
+			
+			
+		}else { 
+			
+			current_titel -= 1;
+			
+		}
+		
 		
 		
 	}
 
+
 	
 	
 	
-	
-	
-	
-	//Print Ausgaben
+	/*
+	 * To String AUsgabe 
+	 * Ausgabe : Playlistname + Anzahl Tracks
+	 */
 	@Override
 	public String toString() {
 		return ("Playlist Name : " + this.pl_name + "\nAnzahl an Tracks : " + this.playlist.size());
 	}
 
+	
+	
+	/*
+	 * To String
+	 * Ausgabe : toString() + alleTitelinformationen 
+	 */
 	public void drucke_playlist() {	
 	System.out.print(this.toString() + "\n");
 	
@@ -44,8 +243,13 @@ public class Playlist {
 		}
 	}
 	
-	//Playlist erstellen / Titel hinzufügen 
 	
+	
+
+	/*
+	 * Playlist nach Kategorie mit Tracks füllen
+	 * 
+	 */
 	public void add_Interpret(String Interpretname) {	
 	//temporärer zwichenspeicher um nach duplicaten zu überprüfen
 	ArrayList<Titel> tmp =	TitelDB.getListInterpret(Interpretname);
@@ -95,9 +299,14 @@ public class Playlist {
 	//add_playlist_to_db(this);	
 	}
 	
-	//abstract void add_Jahrzehnt(int jahrzehnt) {}
+	
+	
+	
 
 	
+	/*
+	 * ToString() für alle erstellten Playlisten
+	 */
 	static void drucke_alle_pl() {
 		System.out.println("Druck DB :\n");
 		
@@ -116,51 +325,37 @@ public class Playlist {
 	 * 
 	 */
 	
+	static void set_previos_track(String s) {
+		 previos_track = s;	
+
+	}
+
+	static String get_previos_track() {
+		
+		return  previos_track;
+		
+		
+	}	
+
 	public String getPl_name() {
 		return pl_name;
 	}
-
-
-
-
 
 	public void setPl_name(String pl_name) {
 		this.pl_name = pl_name;
 	}
 
-
-
-
-
 	public ArrayList<Titel> getPlaylist() {
 		return playlist;
 	}
-
-
-
-
 
 	public void setPlaylist(ArrayList<Titel> playlist) {
 		this.playlist = playlist;
 	}
 
-
-
-
-
-
-
-
 	public static ArrayList<Playlist> getAll_pl() {
 		return all_pl;
 	}
-
-
-
-
-
-
-
 
 	public static void setAll_pl(ArrayList<Playlist> all_pl) {
 		Playlist.all_pl = all_pl;
