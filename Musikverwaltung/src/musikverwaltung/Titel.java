ca6e388681c567e78fe1ac4de449a6bbf8df3172
SharkTupus <77885071+SharkTupus@users.noreply.github.com>
2021-08-07 17:19:47 +0200

    Update Titel.java

--- Musikverwaltung/src/musikverwaltung/Titel.java
+++ Musikverwaltung/src/musikverwaltung/Titel.java
@@ -1,109 +1,93 @@
 package musikverwaltung;
 
 public class Titel {
+
+	//Eigenschaften des Titelobjekts
 	protected String name;
 	protected String interpret;
 	protected String album;
 	protected int jahr;
 	protected String genre;
+	// zum Speichern und Abspielen der Audio-Datei
 	protected String path;
 	public Wave_player player;
-	
-	
-	
-	
-	
-	
-	public Titel (String name, String inter, String album, int jahr, String genre, String path) {
-		
-		
-		
+
+	//Konstruktor
+	public Titel(String name, String inter, String album, int jahr, String genre, String path) { // Konstuktor
+
 		this.name = name;
 		this.interpret = inter;
 		this.album = album;
 		this.jahr = jahr;
 		this.genre = genre;
-		//Audio
-		this.path = "C:\\Users\\charl\\OneDrive\\Desktop\\project\\Musikverwaltung\\musik_titel\\DieWoodys-FichtlsLied.wav"; //Windows Path Beispiel
+		// Audio
+		this.path = path;
 		this.player = new Wave_player(path);
-		
+
 	}
-	
-	public void printMe() {
-		System.out.println(name + "; " + interpret + "; " + album + "; " + jahr + "; " + genre);
-	}
-	
+
+	/*
+	 * \brief toString
+	 * 
+	 * 		Umwandlung der Eigenschaften eines Titels in einen String, genutzt zur Anzeige im Verwaltungsmodus
+	 * 
+	 * \param nichts
+	 * \return String --> Kombination alle Eigenschaften
+	 * \pre Nutzung des Verwaltungs-Modus
+	 */
 	@Override
 	public String toString() {
 		return (name + "; " + interpret + "; " + album + "; " + jahr + "; " + genre);
 	}
-	
-	
-	/*Liefert Anzeige für Titel im Player
+
+	/*
+	 * \brief player_out
 	 * 
+	 * 		Umwandlung des Namen und Interpreten eines Titels in einen String, genutzt zur Anzeige Abspiel-Modus
 	 * 
+	 * \param nichts
+	 * \return String --> Kombination der beiden Eigenschaften
+	 * \pre Nutzung des Abspiel-Modus
 	 */
 	public String player_out() {
-		
-		
 		return (name + " - " + interpret);
-	
 	}
-	
-	public String player_out_bearbeiten() {
-		
-		
-		return(name + " - " + interpret + " - "  + album + " - " + genre);
-		
-		
-	}
-	
 
+	/*
+	 * \brief player_out_bearbeiten
+	 * 
+	 * 		Umwandlung der Eigenschaften eines Titels (außer des Jahres) in einen String, genutzt zur Anzeige im Bearbeiten-Modus
+	 * 
+	 * \param nichts
+	 * \return String --> Kombination der Eigenschaften
+	 * \pre Nutzung des Playlist-Bearbeiten-Modus
+	 */
+	public String player_out_bearbeiten() {
+		return (name + " - " + interpret + " - " + album + " - " + genre);
+	}
+
+	//Getter der Attribute des Titel-Objekts
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getInterpret() {
 		return interpret;
 	}
 
-	public void setInterpret(String interpret) {
-		this.interpret = interpret;
-	}
-
 	public String getAlbum() {
 		return album;
-	}
-
-	public void setAlbum(String album) {
-		this.album = album;
 	}
 	
 	public int getJahr() {
 		return jahr;
 	}
 
-	public void setJahr(int jahr) {
-		this.jahr = jahr;
-	}
-
 	public String getGenre() {
 		return genre;
 	}
 
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
-	
 	public String getPath() {
 		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
 	}
 }
